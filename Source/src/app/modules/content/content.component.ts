1ab7562549c691d749c965ccd86b853b31076484
B-V-Vulkov <vulkov.borislav@gmail.com>
2020-08-31 17:57:25 +0300

    Add tabs in content page

--- Source/src/app/modules/content/content.component.ts
+++ Source/src/app/modules/content/content.component.ts
@@ -1,15 +1,45 @@
 import { Component, OnInit } from '@angular/core';
+import { cwd } from 'process';
 
 @Component({
-  selector: 'app-content',
-  templateUrl: './content.component.html',
-  styleUrls: ['./content.component.scss']
+    selector: 'app-content',
+    templateUrl: './content.component.html',
+    styleUrls: ['./content.component.scss']
 })
+
+
+
 export class ContentComponent implements OnInit {
 
-  constructor() { }
+    public applications: any;
+    public selectedApplication: any;
 
-  ngOnInit() {
-  }
+    public dropdownVisibility: boolean;
 
+    public tabType: number;
+
+    constructor() { }
+
+    ngOnInit() {
+        this.tabType = 1;
+
+        this.applications = [
+            { id: 0, name: "Електронен дневник" },
+            { id: 1, name: "Лични картони" }
+        ];
+
+        this.selectedApplication = this.applications[0];
+
+
+    }
+
+    selectApplication(id: number) {
+        this.selectedApplication = this.applications[id];
+
+        console.log(this.selectedApplication);
+    }
+
+    showOrHideDropdown() {
+        this.dropdownVisibility = !this.dropdownVisibility;
+    }
 }
